e41eb7c79f93a5a15a66ae90f46c8015a9014b78
DEEKSHITH REDDY <itsdeekshit098@gmail.com>
2023-12-29 23:29:27 +0530

    loginform --v2 checking username adn pass and giving acces accordingly

--- A_projects/login_form/app.py
+++ A_projects/login_form/app.py
@@ -5,20 +5,42 @@ app=Flask(__name__)
 
 database_connection={
     "username":"root",
-    "hostname":"127.0.0.1",
+    "host":"127.0.0.1",
     "password":"sql@hansi123mgp",
     "database":"login"
 }
 
 def connection():
-    return sql.connector.connect(**database_connection)
+    return mysql.connector.connect(**database_connection)
 
 @app.route("/")
 def home():
     return render_template("login.html")
 
-@app.route("/index",methods=["POST"])
-def index():
+# @app.route("/index",methods=["POST"])
+# def index():
+#     mail=request.form.get("email")
+#     passwd=request.form.get("passwd")
+#     return render_template("index.html",mail=mail,passwd=passwd)
+
+@app.route("/display",methods=["POST","GET"])
+def display():
     mail=request.form.get("email")
     passwd=request.form.get("passwd")
-    return render_template("index.html",mail=mail,passwd=passwd)
+    conn=connection()
+    cursor=conn.cursor(dictionary=True)
+    query="SELECT * FROM login"
+    cursor.execute(query)
+    records=cursor.fetchall()
+    print(records)
+    cursor.close()
+    conn.close()
+    for i in records:
+        if i["username"]==mail and i["password"]==passwd:
+            return render_template("index.html",id=i["userid"])
+    return render_template("invalid.html")
+            
+    
+                
+    
+
